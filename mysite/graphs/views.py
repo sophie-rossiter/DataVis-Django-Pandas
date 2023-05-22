bbc5d49206d2d2c07795502f059274ca378dedf8
sophie-rossiter <sophrossit@icloud.com>
2023-05-22 13:36:15 +0100

    url mapping

--- mysite/graphs/views.py
+++ mysite/graphs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(response):
+    return HttpResponse("home")
+
+
+def piechart(response):
+    return HttpResponse("piechart")
+
