719aedad8d42dfdcfcc182e2389e2053d4139cd9
sophie-rossiter <sophrossit@icloud.com>
2023-05-23 13:57:24 +0100

    adding models

--- mysite/graphs/models.py
+++ mysite/graphs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class commodities(models.Model):
+    symbol = models.CharField(max_length=200)
+    date = models.DateField()
+    open = models.DecimalField(max_digits=5, decimal_places=2)
+    close = models.DecimalField(max_digits=5, decimal_places=2)
+    volume = models.IntegerField(max_length=10)
+
